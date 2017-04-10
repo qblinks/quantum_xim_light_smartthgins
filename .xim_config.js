f1374dcb2737cf99adccfaff22a952527431d8a7
Yi-Hua Liang <yihua1218@gmail.com>
2017-04-10 19:23:24 +0800

    Added default .xim_config

---
+++ .xim_config.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017
+ * Qblinks Incorporated ("Qblinks").
+ * All rights reserved.
+ *
+ * The information contained herein is confidential and proprietary to
+ * Qblinks. Use of this information by anyone other than authorized employees
+ * of Qblinks is granted only under a written non-disclosure agreement,
+ * expressly prescribing the scope and manner of such use.
+ */
+
+module.exports = {
+  quantum_token: 'MY_QUANTUM_TOKEN',
+  device_id: 'MY_DEVICE_ID',
+};
