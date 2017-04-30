04f42a24f9514694d8c26639f17038f803575f71
colaYang <a7439616andy@gmail.com>
2017-05-01 02:12:19 +0800

    authenticate test

--- authenticate.js
+++ authenticate.js
@@ -11,6 +11,47 @@
 
 'use strict';
 
+const request = require('request');
+/**
+ * get smartthing token
+ * @param {object} [options]
+ * @param {function} tokenCB callback
+ */
+function get_token(options, tokenCB) {
+  const tokenres = {
+    method: 'GET',
+    url: `${process.env.auth_url}/token/smartthings/${options.xim_channel_set}`,
+    headers: {
+      authorization: `Bearer ${options.quantum_token}`,
+    },
+    formData: {} };
+  request(tokenres, (error, response, body) => {
+    if (error) throw new Error(error);
+    const contact = JSON.parse(body);
+    console.log(contact);
+    tokenCB(contact);
+  });
+}
+/**
+ * get smartthing endpoint
+ * @param {string} [access_token] smartthings token
+ * @param {function} callback callback
+ */
+function get_endpoint(access_token, callback) {
+  const tokenres = {
+    method: 'GET',
+    url: 'https://graph.api.smartthings.com/api/smartapps/endpoints',
+    headers: {
+      authorization: `Bearer ${access_token}`,
+    },
+    formData: {} };
+  request(tokenres, (error, response, body) => {
+    if (error) throw new Error(error);
+    const contact = JSON.parse(body);
+    console.log(contact);
+    callback(contact);
+  });
+}
 /**
  * [authenticate description]
  * @param  {oblect}   options  options object created from xim_instance() with the additional
@@ -20,9 +61,23 @@
  */
 function authenticate(options, callback) {
   const callback_options = JSON.parse(JSON.stringify(options));
-  // this is an empty function to be implemented or a place holder
-
-  callback(callback_options);
+  callback_options.result = {};
+  callback_options.xim_content = {};
+  // get token
+  get_token(options, (result) => {
+    if (result.result !== true) {
+      callback_options.result.err_no = 999;
+      callback_options.result.err_msg = 'error';
+      callback(callback_options);
+    }
+    callback_options.xim_content.access_token = result.access_token;
+    get_endpoint(result.access_token, (endpoint) => {
+      callback_options.xim_content.uri = endpoint.uri;
+      callback_options.result.err_no = 0;
+      callback_options.result.err_msg = 'ok';
+      callback(callback_options);
+    });
+  });
 }
 
 /**
--- discovery.js
+++ discovery.js
@@ -11,6 +11,27 @@
 
 'use strict';
 
+const request = require('request');
+
+/**
+ * get the list of smartthings
+ *
+ * @param {object} option the xim option
+ * @param {function} get_state_cb callback of this function
+ */
+function get_list(option, get_state_callback) {
+  const get_state_options = {
+    method: 'GET',
+    url: `${option.xim_content.uri}/switchDiscovery`,
+    headers: {
+      authorization: `Bearer ${option.xim_content.access_token}`,
+    },
+  };
+  request(get_state_options, (error, response, body) => {
+    const contact = JSON.parse(body);
+    get_state_callback(contact);
+  });
+}
 /**
  * for xim interface
  * @param  {object}   option   input xim_content
@@ -19,9 +40,18 @@
 function discovery(options, callback) {
   const callback_option = JSON.parse(JSON.stringify(options));
   // this is an empty function to be implemented or a place holder
-
-  callback(callback_option);
+  callback_option.list = [];
+  get_list(options, (result) => {
+    Object.keys(result.switches).forEach((key) => {
+      const light = {};
+      light.device_name = result.switches[key].devtest.name;
+      light.device_id = result.switches[key].devtest.id;
+      callback_option.list.push(light);
+    });
+    callback(callback_option);
+  });
 }
+
 
 /**
  * functions exporting
