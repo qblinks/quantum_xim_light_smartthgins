2d1f6c9b5611ef14034b2f5626fb373c3bfb0945
colaYang <a7439616andy@gmail.com>
2017-05-08 06:26:58 +0800

    action one-off

--- action.js
+++ action.js
@@ -11,6 +11,31 @@
 
 'use strict';
 
+const request = require('request');
+
+/**
+ * action to api
+ *
+ * @param {object} option the xim option
+ * @param {string} [command] [the action command]
+ * @param {function} callback of this function
+ */
+function goaction(option, callback) {
+  const goaction_options = {
+    method: 'PUT',
+    url: `${option.xim_content.uri}/switchAction/${option.device_id}/${option.command}`,
+    headers: {
+      authorization: `Bearer ${option.xim_content.access_token}`,
+    },
+  };
+  request(goaction_options, (error) => {
+    // const contact = JSON.parse(body);
+    if (error) {
+      callback(false);
+    }
+    callback(true);
+  });
+}
 /**
  * [action description]
  * @param  {object} option Light action properties
@@ -18,9 +43,31 @@
  */
 function action(option, callback) {
   const callback_option = JSON.parse(JSON.stringify(option));
-  // this is an empty function to be implemented or a place holder
-
-  callback(callback_option);
+  callback_option.result = {};
+  // check xim content
+  if (typeof option.xim_content === 'undefined' || typeof option.xim_content.access_token === 'undefined') {
+    callback_option.result.err_no = 2;
+    callback_option.result.err_msg = 'xim_content undefined or access_token undefined';
+    callback(callback_option);
+  }
+  // set onoff
+  if (typeof option.action.onoff !== 'undefined') {
+    if (option.action.onoff === true) {
+      callback_option.command = 'on';
+    } else {
+      callback_option.command = 'off';
+    }
+  }
+  goaction(callback_option, (result) => {
+    if (typeof result !== 'undefined') {
+      callback_option.result.err_no = 0;
+      callback_option.result.err_msg = 'ok';
+    }
+    delete callback_option.device_id;
+    delete callback_option.action;
+    delete callback_option.command;
+    callback(callback_option);
+  });
 }
 
 /**
